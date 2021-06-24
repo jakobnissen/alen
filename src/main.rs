ba7cf8573ef35f64d75a0d595d409b79f1ff3e18
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-06-24 15:16:36 +0200

    Check seq lengths

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::io::{self, BufRead, BufReader};
 struct Alignment {
     names: Vec<String>,
     seqs: Vec<Vec<u8>>,
-}    
+}
 
 impl Alignment {
     fn new<T: BufRead>(file: T) -> Alignment {
@@ -20,6 +20,7 @@ impl Alignment {
         let mut names: Vec<String> = Vec::new();
         let reader = fasta::Reader::new(file);
         let alphabet = alphabets::Alphabet::new(b"-ACMGRSVTWYHKDBNacmgrsvtwyhkdbn");
+        let mut seqlength: Option<usize> = None;
         for result in reader.records() {
             // TODO: Better error message - file nume and record number, perhaps
             let record = result.expect("Error during FASTA parsing");
@@ -27,13 +28,22 @@ impl Alignment {
             if !alphabet.is_word(&seq) {
                 panic!("Error: Sequence cannot be understood as DNA") // TODO: More precise error
             }
+
+            // Check identical sequence lengths
+            if let Some(len) = seqlength {
+                if seq.len() != len {
+                    panic!("Error: Sequence lengths uneven") // TODO: More precise error
+                }
+            } else {
+                seqlength = Some(seq.len())
+            }
             seqs.push(seq);
             names.push(record.id().to_owned());
         }
         // TODO: Warn if two headers are identical
         Alignment{names, seqs}
-    }    
-}    
+    }
+}
 
 fn main() {
     let buffered = BufReader::new(io::stdin());
